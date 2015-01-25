373c2d53c398c8fc9a1c7f5aa26605b8c611c4a7
lloydbenson <lloyd.benson@gmail.com>
2015-01-25 10:10:34 -0600

    checkin

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# smelt
-simple command functions
+smelt
+=======
+
+simple command runner
+
+[![Build Status](https://secure.travis-ci.org/fishin/smelt.svg)](http://travis-ci.org/fishin/smelt)
+[![NPM version](https://badge.fury.io/js/smelt.svg)](http://badge.fury.io/js/smelt)
+[![devDependency Status](https://david-dm.org/fishin/smelt/dev-status.svg)](https://david-dm.org/fishin/smelt#info=devDependencies)
+
+Need to write up some doc when we are done.
+
---
+++ test/command.js
@@ -0,0 +1,54 @@
+var Code = require('code');
+var Lab = require('lab');
+
+var Smelt = require('../lib/index');
+
+var lab = exports.lab = Lab.script();
+var expect = Code.expect;
+var describe = lab.describe;
+var it = lab.it;
+
+var internals = {
+    defaults: {
+        dirPath: 'tmp'
+    }
+};
+
+describe('command', function () {    
+
+    it('runCommand valid', function (done) {
+
+        var smelt = new Smelt(internals.defaults);
+        var command = 'uptime';
+        smelt.runCommand(command, function(result) {
+
+            expect(result.status).to.equal('succeeded');
+            expect(result.stdout).to.exist();
+            done();
+        });
+    });
+
+    it('runCommand invalid', function (done) {
+
+        var smelt = new Smelt(internals.defaults);
+        var command = 'invalid';
+        smelt.runCommand(command, function(result) {
+
+            expect(result.status).to.equal('failed');
+            expect(result.error).to.exist();
+            done();
+        });
+    });
+
+    it('runCommand failed', function (done) {
+
+        var smelt = new Smelt(internals.defaults);
+        var command = 'ls lloyd';
+        smelt.runCommand(command, function(result) {
+
+            expect(result.status).to.equal('failed');
+            expect(result.stderr).to.exist();
+            done();
+        });
+    });
+});
